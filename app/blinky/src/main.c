11a678b59ad3d6e82fc8295712326835b73de14f
Jean-Luc Watson <jlwatson@eecs.berkeley.edu>
2019-08-06 17:07:23 -0700

    blinky app secure faulting on timers

---
+++ app/blinky/src/main.c
@@ -0,0 +1,33 @@
+#include "interface.h"
+
+unsigned int blink_state = 0;
+struct k_timer t;
+
+void timer_expire_cb(struct k_timer *t) {
+    printk("hello?\n");
+    blink_state ^= 1;
+    printk("setting state to %d\n", blink_state);
+    gpio_write(2, blink_state);
+}
+
+void timer_stop_cb(struct k_timer *t) {
+    printk("timer stopped\n");
+}
+
+void main(void) {
+
+    gpio_enable_output(2);
+
+    /*
+    while(1) {
+        blink_state ^= 1;
+        gpio_write(2, blink_state);
+
+        k_sleep(500);
+    }
+    */
+
+    k_timer_init(&t, timer_expire_cb, timer_stop_cb);
+    k_timer_start(&t, 500, 500);
+}
+
