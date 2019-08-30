c13f7ae94f8d354d4de7c7cca04511338abb2f25
Sherry Luo <sherry.z.luo123@berkeley.edu>
2019-08-30 12:09:39 -0700

    getting error in relocate.py

--- app/glucose_monitoring/src/main.c
+++ app/glucose_monitoring/src/main.c
@@ -19,17 +19,13 @@
 #define REQUIRED_TEMP(bg) (SCHEDULED_BASAL + (bg - TARGET) / (ISF * MINUTES_PER_APPLICATION))
 
 int calculate_next_temp(int current_bg, int delta, int existing_basal, int expected_bg) {
-  printk("Val1: %d", current_bg);
-  printk("Val2: %d", delta);
-  printk("Val3: %d", existing_basal);
-  printk("Val4: %d", expected_bg);
-  /*if (current_bg < TARGET - THRESHOLD) {
+  if (current_bg < TARGET - THRESHOLD) {
     return 0;
   } else if (delta > 0 && expected_bg < TARGET) {
     return SCHEDULED_BASAL;
   } else if (delta < 0 && expected_bg > TARGET) {
     return SCHEDULED_BASAL;
-  } else { //if (expected_bg > TARGET)
+  } else if (expected_bg > TARGET) {
     int required_temp = REQUIRED_TEMP(expected_bg);
     if (required_temp > existing_basal) {
       return required_temp < 0? 0 : required_temp;
@@ -39,10 +35,10 @@ int calculate_next_temp(int current_bg, int delta, int existing_basal, int expec
         return required_temp < 0? 0 : required_temp; // if >30m @ 0 required, extend zero temp to 30m
       }
     }
-  }*/
+  }
   return existing_basal;
 }
-/*
+
 int calculate_net_iob(int insulin_intake[], int length, int beginning_index) {
   int peak = MINUTES_TO_PEAK_FOR_DEFAULT_DIA * DIA / DEFAULT_DIA;
   int minutes, insulin, net_iob = 0;
@@ -57,7 +53,7 @@ int calculate_net_iob(int insulin_intake[], int length, int beginning_index) {
   }
   return net_iob;
 }
-*/
+
 int get_next_bg(int i) {
   return (10 - i) * i * 10; // TODO: Replace with pin reading
 }
@@ -65,18 +61,18 @@ int get_next_bg(int i) {
 void main(void) {
   int length = DIA / MINUTES_PER_APPLICATION;
   int insulin_intake[length];
-  int beginning_index = length - 1, prev_bg = 0, current_bg = 0, existing_basal = SCHEDULED_BASAL;
+  int beginning_index = length - 1, prev_bg = 0, current_bg = 0, expected_bg = 0, existing_basal = SCHEDULED_BASAL;
   for (int i = 0; i < length; i++) {
     insulin_intake[i] = 0;
   }
   // TODO: Replace with timer functions
   for (int i = 1; i < 11; i++) {
     current_bg = get_next_bg(i);
-    existing_basal = calculate_next_temp(current_bg, current_bg - prev_bg, 3, 4); //calculate_net_iob(insulin_intake, length, beginning_index), , , 100
+    expected_bg = current_bg - ISF * calculate_net_iob(insulin_intake, length, beginning_index);
+    existing_basal = calculate_next_temp(current_bg, current_bg - prev_bg, existing_basal, expected_bg);
     existing_basal = MIN(existing_basal, MAX_BASAL_PUMP);
-    printk("Input: %d; Output: %d.%d\n", current_bg/10, existing_basal/10, existing_basal%10);
     prev_bg = current_bg;
-    // beginning_index = beginning_index == 0? length - 1 : beginning_index - 1;
-    // insulin_intake[beginning_index] = existing_basal * MINUTES_PER_APPLICATION;
+    beginning_index = beginning_index == 0? length - 1 : beginning_index - 1;
+    insulin_intake[beginning_index] = existing_basal * MINUTES_PER_APPLICATION;
   }
 }
