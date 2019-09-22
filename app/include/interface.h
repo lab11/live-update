876ea2feaf326b52600029a95261ef23dd59ce9f
Saharsh Agrawal <saharshagrawal@berkeley.edu>
2019-09-21 17:15:58 -0700

    commented code temporarily

--- app/include/interface.h
+++ app/include/interface.h
@@ -10,10 +10,12 @@ struct k_timer {
     char contents[44];
 };
 
+// void observe(char* event);
+
 typedef void (*k_timer_expiry_t)(struct k_timer *t);
 typedef void (*k_timer_stop_t)(struct k_timer *t);
 extern void (*k_timer_init)(struct k_timer *t, k_timer_expiry_t exp_fn, k_timer_stop_t stop_fn);
 extern void (*k_timer_start)(struct k_timer *t, int duration, int period);
 extern void (*k_timer_stop)(struct k_timer *t);
 
-extern K_THREAD_DEFINE(name, stack_size, entry, p1, p2, p3, prio, options, delay);
+// extern K_THREAD_DEFINE(name, stack_size, entry, p1, p2, p3, prio, options, delay);
