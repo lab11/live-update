84105bac7b5a4bb43cf4d1ba9d5e68ca68a27228
Sherry Luo <sherry.z.luo123@berkeley.edu>
2019-10-27 22:42:10 -0700

    began general structure of initial implementation

---
+++ app/wind_turbine/src/main.c
@@ -0,0 +1,37 @@
+#include "interface.h"
+#include <math.h> // Can we include math.h?
+
+#define P_NOM 1 // Nominal power; Must determine value
+#define J_T 1 // Turbine total inertia; Must determine value
+#define R 1 // Rotor radius; Must determine value
+
+#define RHO 1 // Air density; Must determine value
+#define PI 3.14159265358979323846 // Replace with math.h's pi
+
+float calculate_power_coefficient (float tip_speed_ratio, float pitch_angle) {
+  // WT_PERF by NREL; Must investigate
+}
+
+float calculate_aerodynamic_torque (float rotor_speed, float pitch_angle, float wind_speed) {
+  // Section 2.1
+  float tip_speed_ratio = rotor_speed * R / wind_speed; // Equation (2)
+  return RHO * PI * pow(R, 3)
+      * calculate_power_coefficient (tip_speed_ratio, pitch_angle)
+      * pow (wind_speed, 2) / 2; // Equation (4)
+}
+
+float calculate_generator_torque (float rotor_speed, float electrical_power) {
+  // Section 3.2.1
+  float power_tracking_error = P_NOM - electrical_power; // Must calc c_0; When deriv + val * c_0 = 0
+  // Following for simple quadratic formula to solve equation (15)
+  float a_const = 1 / (rotor_speed * J_T);
+  float b_const = - calculate_aerodynamic_torque (/* Must calculate variables or collect input*/) / (rotor_speed * J_T)
+      - rotor_speed * K_T /* Must calculate K_T */ / (rotor_speed * J_T) - 1;
+  float c_const = c_0 * power_tracking_error / rotor_speed;
+
+  return (- b_const + sqrt (pow (b_const, 2) - 4 * a_const * c_const)) / (2 * a_const); // Which value will be used? +/- version?
+}
+
+void main(void) {
+
+}
