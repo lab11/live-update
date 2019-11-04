442ab922736cf4c9023a7d7b7de43703833dd187
Sherry Luo <sherry.z.luo123@berkeley.edu>
2019-11-04 09:52:18 -0800

    power coefficient

--- app/wind_turbine/src/main.c
+++ app/wind_turbine/src/main.c
@@ -8,11 +8,24 @@
 #define RHO 1 // Air density; Must determine value
 #define PI 3.14159265358979323846 // Replace with math.h's pi
 
+ /*
+ Should be constant for particular wind turbine but perhaps can update
+ these if necessary?
+ */
+static float constants[] = {1, 1, 1, 1, 1, 1}, x_constant = 1;
 float calculate_power_coefficient (float tip_speed_ratio, float pitch_angle) {
-  // WT_PERF by NREL; Must investigate
+  // WT_PERF by NREL
+  float relational_constant = 1 / (tip_speed_ratio + 0.08 * pitch_angle)
+  - 0.035 / (1 + pow (pitch_angle, 3));
+  return constants[0] * (constants[1] * relational_constant
+    - constants[2] * pitch_angle
+    - constants[3] * pow (pitch_angle, x)
+    - constants[4]) * exp (-constants[5] * relational_constant);
+
 }
 
-float calculate_aerodynamic_torque (float rotor_speed, float pitch_angle, float wind_speed) {
+float calculate_aerodynamic_torque (float rotor_speed, float pitch_angle,
+  float wind_speed) {
   // Section 2.1
   float tip_speed_ratio = rotor_speed * R / wind_speed; // Equation (2)
   return RHO * PI * pow(R, 3)
